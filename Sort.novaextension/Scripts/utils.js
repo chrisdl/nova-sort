4c0cffccb3571eca4197aed8a6bdf838af2394b2
Chris DiLorenzo <dilorenzo.christopher@gmail.com>
2020-11-19 23:53:59 -0500

    add linting and apply the fixes. (#2)

--- Sort.novaextension/Scripts/utils.js
+++ Sort.novaextension/Scripts/utils.js
@@ -1,21 +1,20 @@
-function lastInArrayIsEmptyString(arr) {
+function lastInArrayIsEmptyString (arr) {
   return arr.slice(-1)[0] === ''
 }
 
-function sortByLineLength(a, b) {
-  a = a.trim();
-  b = b.trim();
-  if (a.length === b.length) return 0;
-  return a.length - b.length;
+function sortByLineLength (a, b) {
+  a = a.trim()
+  b = b.trim()
+  if (a.length === b.length) return 0
+  return a.length - b.length
 }
 
-function sortByLineLengthReversed(a, b) {
-  a = a.trim();
-  b = b.trim();
-  if (a.length === b.length) return 0;
-  return b.length - a.length;
+function sortByLineLengthReversed (a, b) {
+  a = a.trim()
+  b = b.trim()
+  if (a.length === b.length) return 0
+  return b.length - a.length
 }
-
 
 module.exports = {
   lastInArrayIsEmptyString,
